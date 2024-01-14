646107cafd4a55c6d8c9adeeb35cfe8054d8420f
SmortBoy65 <92156281+SmortBoy65@users.noreply.github.com>
2024-01-14 11:13:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tnshort.net')
 SHORTLINK_API = environ.get('SHORTLINK_API', '6ebc051fc1a944e329fe6e7567ba43ad819b9258')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
