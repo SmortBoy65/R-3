b870fb05ef4042625feadb274b652f98e615bbef
Pdisk <83772019+pdisk@users.noreply.github.com>
2024-01-18 00:49:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -182,7 +182,7 @@ async def start(client, message):
                           InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                        ],#[ 
                             #InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @LazyDeveloperr
-                            ]
+                            #]
                         ]
                     )
                 )
