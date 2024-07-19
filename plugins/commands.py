5972cc1c054f2ec9641853b160056dda7782289a
SmortBoy65 <92156281+SmortBoy65@users.noreply.github.com>
2024-07-19 22:18:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,8 +30,8 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url="https://t.me/R_3Group"),
-                    InlineKeyboardButton('Mᴏᴠɪᴇ ɢʀᴏᴜᴘ', url='https://t.me/R_3Group')
+                    InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url="https://t.me/R_3Group1"),
+                    InlineKeyboardButton('Mᴏᴠɪᴇ ɢʀᴏᴜᴘ', url='https://t.me/R_3Group1')
                 ],[
                     InlineKeyboardButton('Jᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ', url=CHNL_LNK)
                   ]]
@@ -51,7 +51,7 @@ async def start(client, message):
                     InlineKeyboardButton('Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
                     InlineKeyboardButton('Eᴀʀɴ ᴍᴏɴᴇʏ ', callback_data="shortlink_info"),
-                    InlineKeyboardButton(' Mᴏᴠɪᴇ ɢʀᴏᴜᴘ', url='https://t.me/R_3Group')
+                    InlineKeyboardButton(' Mᴏᴠɪᴇ ɢʀᴏᴜᴘ', url='https://t.me/R_3Group1')
                 ],[
                     InlineKeyboardButton(' Hᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('Aʙᴏᴜᴛ', callback_data='about')
